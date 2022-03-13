e61c3b5ac93db3ae2b1052d6345b15e02bd8d93c
hiepxuan2006 <hiepxuan2006@gmail.com>
2022-03-13 21:10:18 +0700

    fix

--- src/Context/ContTant.js
+++ src/Context/ContTant.js
@@ -1,3 +1,3 @@
 export const baseURL = 'https://mighty-dusk-66790.herokuapp.com/upload'
-export const baseApi = process.env.NODE_ENV !== 'production' ? 'https://mighty-dusk-66790.herokuapp.com/api' : 'http://localhost:5000/api'
+export const baseApi = 'https://mighty-dusk-66790.herokuapp.com/api'
 export const TOKEN_NAME = 'token-name'
